21ed769cd5c537bef8a7a69bb313c0b56d10527e
MohamedMansour03 <mansour01medo@gmail.com>
2023-11-02 15:56:20 +0100

    index.js

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Form from './Components/Form'
+
+
+
+import  './App.css'
+
+
+function App() {
+
+
+    
+
+
+  return (
+    <div className="container">
+    
+   
+    
+    </div>
+  );
+}
+
+export default App;
